89757449d3baeb55adf3d52fa45640f711bf8111
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-04-06 21:52:41 -0700

    Complete functional stack and queue

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -4,15 +4,33 @@ var Queue = function() {
   // Use an object with numeric keys to store values
   var storage = {};
 
-  // Implement the methods below
+  // keep pointer to the index of first and last item
+  var first_index = 0;
+  var last_index = 0;
 
   someInstance.enqueue = function(value) {
+    // add new item to storage
+    storage[last_index] = value;
+    // increment last index
+    last_index++;
   };
 
   someInstance.dequeue = function() {
+    // if empty return
+    if (last_index-first_index <= 0) return;
+    // store deleted item
+    var deleted = storage[first_index];
+    // delete item
+    delete storage[first_index];
+    // increment first index
+    first_index++;
+    // return deleted item
+    return deleted;
   };
 
   someInstance.size = function() {
+    // return last - first
+    return last_index-first_index
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,17 +1,33 @@
 var Stack = function() {
   var someInstance = {};
 
-  // Use an object with numeric keys to store values
   var storage = {};
 
-  // Implement the methods below
+  // keep count variable to know how many items we have
+  var count = 0;
+
   someInstance.push = function(value) {
+    // adding a value to the stack obj
+    storage[count] = value;
+    // increasing count
+    count++;
   };
 
   someInstance.pop = function() {
+    if (count === 0) return;
+    // decrease count
+    count--;
+    // store item to delete to return
+    var deleted = storage[count];
+    // delete item
+    delete storage[count];
+    // return deleted item
+    return deleted;
   };
 
   someInstance.size = function() {
+    // return count
+    return count;
   };
 
   return someInstance;
