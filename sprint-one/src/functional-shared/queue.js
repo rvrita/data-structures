52ff03300b92e66e5e16c85a48108a85c677af5d
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-04-07 18:36:02 -0700

    Complete functional-shared stack and queue

--- sprint-one/src/functional-shared/queue.js
+++ sprint-one/src/functional-shared/queue.js
@@ -1,8 +1,44 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // initiate obj
+  var obj = {};
+  // make an empty storage
+  obj.storage = {};
+  // store index of first item
+  obj.first_index = 0;
+  // store index of last item
+  obj.last_index = 0;
+  // connect obj and methods
+  extend(obj, queueMethods);
+  // return obj
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue(value) {
+    // add new item to storage
+    this.storage[this.last_index] = value;
+    // increment last index
+    this.last_index++;
+  },
+  dequeue() {
+  // if empty return
+  if (this.last_index-this.first_index <= 0) return;
+  // store deleted item
+  var deleted = this.storage[this.first_index];
+  // delete item
+  delete this.storage[this.first_index];
+  // increment first index
+  this.first_index++;
+  // return deleted item
+  return deleted;
+  },
+  size() {
+    // return last_index
+    return this.last_index-this.first_index;
+  }
+};
 
 
--- sprint-one/src/functional-shared/stack.js
+++ sprint-one/src/functional-shared/stack.js
@@ -1,8 +1,47 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // new obj
+  var obj = {};
+  // initiate storage
+  obj.storage = {};
+  // make count variable
+  obj.count = 0;
+  // extend obj with methods
+  extend(obj, stackMethods);
+  // return new obj
+  return obj;
 };
 
-var stackMethods = {};
+var extend = function(obj, stackMethods) {
+  for (var key in stackMethods) {
+    obj[key] = stackMethods[key]
+  }
+}
+
+var stackMethods = {
+  push: function(value) {
+    // add value to storage
+    this.storage[this.count] = value;
+    // increment counter
+    this.count++;
+  },
+  pop: function() {
+    // return if empty
+    if (this.count === 0) return;
+    // decrement counter
+    this.count--;
+    // save last item
+    var deleted = this.storage[this.count];
+    // delete last item
+    delete this.storage[this.count];
+    // return deleted item
+    return deleted;
+  },
+  size: function() {
+    // return count
+    return this.count;
+  }
+};
 
 
