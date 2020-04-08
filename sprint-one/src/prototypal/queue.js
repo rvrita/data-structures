e9b5070834945bbabd2812b9d8ffb17892b6fcaf
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-04-07 18:40:27 -0700

    Complete prototypal stack and queue

--- sprint-one/src/prototypal/queue.js
+++ sprint-one/src/prototypal/queue.js
@@ -1,8 +1,40 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  // initiate new obj
+  var obj = Object.create(queueMethods);
+  // add empty storage obj
+  obj.storage = {};
+  // add first  and last index variable
+  obj.firstIndex = 0;
+  obj.lastIndex = 0;
+  // return new obj instance
+  return obj;
 };
 
-var queueMethods = {};
+var queueMethods = {
+  enqueue(value) {
+    // add new item to storage
+    this.storage[this.lastIndex] = value;
+    // increment last index
+    this.lastIndex++;
+  },
+  dequeue() {
+    // if empty return
+    if (this.lastIndex - this.firstIndex <= 0) { return; }
+    // store deleted item
+    var deleted = this.storage[this.firstIndex];
+    // delete item
+    delete this.storage[this.firstIndex];
+    // increment first index
+    this.firstIndex++;
+    // return deleted item
+    return deleted;
+  },
+  size() {
+    // return last_index
+    return this.lastIndex - this.firstIndex;
+  }
+};
 
 
--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,41 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+
+  // initiate new obj
+  var obj = Object.create(stackMethods);
+  // add empty storage obj
+  obj.storage = {};
+  // add count variable
+  obj.count = 0;
+  // return new obj instance
+  return obj;
 };
 
-var stackMethods = {};
+
+var stackMethods = {
+  push: function(value) {
+    // storing the value
+    this.storage[this.count] = value;
+    // increment counter
+    this.count++;
+  },
+  pop: function() {
+    // check if empty
+    if (this.count === 0) { return; }
+    // decrement counter
+    this.count--;
+    // save last item
+    var deleted = this.storage[this.count];
+    // delete
+    delete this.storage[this.count];
+    // return deleted
+    return deleted
+  },
+  size: function() {
+    // return counter
+    return this.count;
+  }
+};
 
 
