095b95a0afbedcb1feb117dc9da7c504d9cbf9db
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-04-12 17:57:22 -0700

    Complete Tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -2,8 +2,8 @@ var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
 
-  // your code here
-  newTree.children = null;  // fix me
+  _.extend(newTree, treeMethods);
+  newTree.children = [];
 
   return newTree;
 };
@@ -11,13 +11,32 @@ var Tree = function(value) {
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  var child = Tree(value);
+  this.children.push(child);
 };
 
 treeMethods.contains = function(target) {
+  if (this.value === target) {
+    return true;
+  }
+
+  if (this.children.length > 0) {
+    for (var i = 0; i < this.children.length; i++) {
+      self = this.children[i];
+      if (self.contains(target)) {
+        return true;
+      }
+    }
+
+  }
+  return false;
 };
 
-
-
 /*
- * Complexity: What is the time complexity of the above functions?
+
+Complexity: What is the time complexity of the above functions?
+
+addChild: constant time complexity, O(1)
+contains: linear time complexity, O(n)
+
  */
