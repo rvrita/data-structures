b6fb5161356b59d948361fba7a1a62bacb91276e
Rita Rozsavolgyi <rvrita.me@gmail.com>
2020-04-12 17:58:33 -0700

    Complete Set

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,20 +1,33 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set.storage = []; // fix me
   return set;
 };
 
 var setPrototype = {};
 
-setPrototype.add = function(item) {
+setPrototype.contains = function(item) {
+  return this.storage.indexOf(item) === -1 ? false : true;
 };
 
-setPrototype.contains = function(item) {
+setPrototype.add = function(item) {
+  if (!this.contains(item)) {
+    this.storage.push(item);
+  }
 };
 
 setPrototype.remove = function(item) {
+  var index = this.storage.indexOf(item);
+  if (index !== -1) {
+    this.storage.splice(index, 1);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
+
+contains: linear time complexity, O(n)
+add: linear time complexity, O(n)
+remove: linear time complexity, O(n)
+
  */
