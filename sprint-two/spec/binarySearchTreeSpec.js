c49e2320aa379f3689c38f88bfc94e72939323d8
Dominic Silvia <dominic.silvia@gmail.com>
2020-04-19 15:27:22 -0700

    Complete Binary Search Tree

--- sprint-two/spec/binarySearchTreeSpec.js
+++ sprint-two/spec/binarySearchTreeSpec.js
@@ -1,17 +1,17 @@
-describe('binarySearchTree', function() {
+describe('binarySearchTree', function () {
   var binarySearchTree;
 
-  beforeEach(function() {
+  beforeEach(function () {
     binarySearchTree = BinarySearchTree(5);
   });
 
-  it('should have methods named "insert", "contains", and "depthFirstLog', function() {
+  it('should have methods named "insert", "contains", and "depthFirstLog', function () {
     expect(binarySearchTree.insert).to.be.a('function');
     expect(binarySearchTree.contains).to.be.a('function');
     expect(binarySearchTree.depthFirstLog).to.be.a('function');
   });
 
-  it('should insert values at the correct location in the tree', function() {
+  it('should insert values at the correct location in the tree', function () {
     binarySearchTree.insert(2);
     binarySearchTree.insert(3);
     binarySearchTree.insert(7);
@@ -20,7 +20,7 @@ describe('binarySearchTree', function() {
     expect(binarySearchTree.right.left.value).to.equal(6);
   });
 
-  it('should have a working "contains" method', function() {
+  it('should have a working "contains" method', function () {
     binarySearchTree.insert(2);
     binarySearchTree.insert(3);
     binarySearchTree.insert(7);
@@ -28,13 +28,39 @@ describe('binarySearchTree', function() {
     expect(binarySearchTree.contains(8)).to.equal(false);
   });
 
-  it('should execute a callback on every value in a tree using "depthFirstLog"', function() {
+  it('should execute a callback on every value in a tree using "depthFirstLog"', function () {
     var array = [];
-    var func = function(value) { array.push(value); };
+    var func = function (value) { array.push(value); };
     binarySearchTree.insert(2);
     binarySearchTree.insert(3);
     binarySearchTree.insert(7);
     binarySearchTree.depthFirstLog(func);
     expect(array).to.eql([5, 2, 3, 7]);
   });
+
+  it('should delete a node from the tree', function () {
+    binarySearchTree.insert(2);
+    //expect(binarySearchTree.left.value).to.equal(2);
+    binarySearchTree.insert(3);
+    binarySearchTree.insert(1);
+    //binarySearchTree.remove(1);
+    //expect(binarySearchTree.contains(1)).to.equal(false);
+    binarySearchTree.insert(18);
+    binarySearchTree.insert(17);
+    binarySearchTree.insert(20);
+    //binarySearchTree.insert(25);
+    //binarySearchTree.insert(23);
+    binarySearchTree.remove(18);
+    expect(binarySearchTree.contains(18)).to.equal(false);
+    expect(binarySearchTree.contains(17)).to.equal(true);
+    expect(binarySearchTree.contains(20)).to.equal(true);
+    // expect(binarySearchTree.contains(25)).to.equal(true);
+    // expect(binarySearchTree.contains(23)).to.equal(true);
+
+
+
+
+  });
+
+
 });
--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -69,11 +69,68 @@ BinarySearchTree.prototype.depthFirstLog = function (cb) {
   }
 };
 
+BinarySearchTree.prototype.remove = function (value) {
+
+  //set flag for if the item was removed
+  var itemRemoved = false;
+
+  //if tree has nodes on right side of current node
+  if (this.right) {
+    //if chile of current node is the value to remove
+    if (this.right.value === value) {
+      if (this.right.right === null && this.right.left === null) {
+        //if value has no children, remove value by setting parent = null
+        this.right = null;
+        itemRemoved = true;
+      } else if (this.right.right !== null && this.right.left !== null) {
+        //if value has two children
+        this.right.left.right = this.right.right;
+        this.right = this.right.left;
+        itemRemoved = true;
+      } else if (this.right.right === null || this.right.left === null) {
+        //if value has one child
+        this.right = (this.right.right === null) ? this.right.left : this.right.right;
+        itemRemoved = true;
+      }
+    }
+  }
+
+  //if tree has values on the left side
+  if (this.left) {
+    if (this.left.value === value) {
+      if (this.left.right === null && this.left.left === null) {
+        //if value has no children, remove value by setting parent = null
+        this.left = null;
+        itemRemoved = true;
+      } else if (this.left.right !== null && this.left.left !== null) {
+        //if value has two children
+        this.left.right.left = this.left.left;
+        this.left = this.left.right;
+        itemRemoved = true;
+      } else if (this.left.right === null || this.left.left === null) {
+        //if value has one child
+        this.left = (this.left.right === null) ? this.left.left : this.left.right;
+        itemRemoved = true;
+      }
+    }
+  }
+
+  //if value has not been found yet, use recursion to find/remove
+  if (!itemRemoved) {
+    if (value > this.value) {
+      return this.right.remove(value);
+    } else {
+      return this.left.remove(value);
+    }
+  }
+};
+
 /*
  * Complexity: What is the time complexity of the above functions?
  *
  * insert: O(n)
  * contains: O(n)
  * depthFirstLog: O(n)
+ * Remove: O(n)
  *
  */
